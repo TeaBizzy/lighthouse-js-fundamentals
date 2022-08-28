c4df7664e4852a0593c3856caa4544f323915ee4
Stefan <satalbot@protonmail.com>
2022-08-28 16:50:50 +0000

    Use if statements to determine where to go to school

---
+++ which-school-assignment.js
@@ -0,0 +1,13 @@
+const age = 19;
+
+const whichSchool  = function (age) {
+  if(age < 13) {
+    return "Elementary School";
+  } else if(age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs"
+  }
+}
+
+console.log(whichSchool(age));
