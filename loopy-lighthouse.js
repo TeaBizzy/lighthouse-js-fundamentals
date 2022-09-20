eae49ba39f89782f69d74802d2968b678ca04c47
Stefan <satalbot@protonmail.com>
2022-09-20 14:02:24 +0000

    Refactor to allow user to define the range, multiples, and words

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,16 +1,21 @@
-for(let i = 100; i <= 200; i++) {
-  let loopy = "";
-  let lighthouse = "";
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"])
 
-  if(i % 3 === 0) {
-    loopy = "Loopy";
-  }
-  if(i % 4 === 0) {
-    lighthouse = "Lighthouse"
-  }
-  if(i % 3 !== 0 && i % 4 !== 0) {
-    console.log(i);
-  } else {
-    console.log(loopy + lighthouse);
+function loopyLighthouse(range, multiples, words) {
+  for(let i = range[0]; i <= range[1]; i++) {
+    let word1 = "";
+    let word2 = "";
+
+    if(i % multiples[0] === 0) {
+      word1 = words[0];
+    }
+    if(i % multiples[1] === 0) {
+      word2 = words[1];
+    }
+
+    if(i % multiples[0] !== 0 && i % multiples[1] !== 0 ) {
+      console.log(i);
+    } else {
+      console.log(word1 + word2)
+    }
   }
 }
