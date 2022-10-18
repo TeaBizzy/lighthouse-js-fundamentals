c866a9c17ffe87efcc2995cfeb96d04a6dd0bd30
Stefan <satalbot@protonmail.com>
2022-10-18 03:07:30 +0000

    Corrected eslint issues

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,21 +1,21 @@
-loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"])
-
-function loopyLighthouse(range, multiples, words) {
-  for(let i = range[0]; i <= range[1]; i++) {
+const loopyLighthouse = function(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
     let word1 = "";
     let word2 = "";
 
-    if(i % multiples[0] === 0) {
+    if (i % multiples[0] === 0) {
       word1 = words[0];
     }
-    if(i % multiples[1] === 0) {
+    if (i % multiples[1] === 0) {
       word2 = words[1];
     }
 
-    if(i % multiples[0] !== 0 && i % multiples[1] !== 0 ) {
+    if (i % multiples[0] !== 0 && i % multiples[1] !== 0) {
       console.log(i);
     } else {
-      console.log(word1 + word2)
+      console.log(word1 + word2);
     }
   }
-}
+};
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
